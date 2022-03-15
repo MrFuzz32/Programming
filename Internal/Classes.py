ce5419d5b07773b55c2eb4c82ff97abf8c8d7f5f
Bill Campbell <bill@devworks.co.nz>
2022-03-16 09:11:36 +1300

    Completed draft of card class

--- Internal/Classes.py
+++ Internal/Classes.py
@@ -1,10 +1,17 @@
 suits = {1:'Clubs',2:'Spades',3:'Hearts',4:'Diamonds'}
 
 class Card:
-    def __init__(self, number, suitNum):
+    def __init__(self, number, suitNum, faceUp = True):
         self.number = number
         self.suitNum = suitNum
-        self.suit = suits[suitNum]
+        self.faceUp = faceUp
+        
+    def flip(self):
+        self.faceUp = not self.faceUp
+        return self.faceUp
     
-    def summary(self):
-        return [self.number,self.suitNum,self.suit]
+    def red(self):
+        return (self.suitNum == 3 or self.suitNum == 4)
+    
+    def black(self):
+        return (self.suitNum == 1 or self.suitNum == 2)
